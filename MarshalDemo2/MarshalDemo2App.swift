c6b18eca5142e0f50e9569bea9e097c8cdde24fa
Peter Larsson <peter@sakkunskap.se>
2024-09-21 18:24:08 +0200

    Initial Commit

---
+++ MarshalDemo2/MarshalDemo2App.swift
@@ -0,0 +1,17 @@
+//
+//  MarshalDemo2App.swift
+//  MarshalDemo2
+//
+//  Created by Peter Larsson on 2024-09-21.
+//
+
+import SwiftUI
+
+@main
+struct MarshalDemo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MarshalDemo2Tests/MarshalDemo2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  MarshalDemo2Tests.swift
+//  MarshalDemo2Tests
+//
+//  Created by Peter Larsson on 2024-09-21.
+//
+
+import Testing
+@testable import MarshalDemo2
+
+struct MarshalDemo2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
